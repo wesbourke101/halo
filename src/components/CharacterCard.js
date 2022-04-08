ed2248ff9a6413e3a7d603b43091f216f2f39ee9
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-08 15:53:59 -0400

    changes

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,13 @@
+
+function CharacterCard(){
+    return(   
+    <div className="card">
+        <div className="card-image">
+
+        </div>
+
+    </div>
+    )
+}
+
+export default CharacterCard;
